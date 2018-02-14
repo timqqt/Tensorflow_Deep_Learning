cab61c43b5032f14a7eebe0a41150478cd7a523f
timqqt <36435839+timqqt@users.noreply.github.com>
2018-02-14 17:00:52 +0800

    It is about RNN

---
+++ Examples_RNN.py
@@ -0,0 +1,72 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+learning_rate = 0.001
+training_iters = 100000
+batch_size = 128
+
+
+n_inputs = 28
+n_steps = 28
+n_hidden_units = 128
+n_classes = 10
+
+x = tf.placeholder(tf.float32, [None, n_steps, n_inputs])
+y = tf.placeholder(tf.float32, [None, n_classes])
+
+# hidden layers surround the cell
+weights = {
+    'in': tf.Variable(tf.random_normal([n_inputs, n_hidden_units])),
+    'out': tf.Variable(tf.random_normal([n_hidden_units, n_classes]))
+}
+
+
+biases = {
+    'in': tf.Variable(tf.constant(0.1, shape=[n_hidden_units, ])),
+    'out': tf.Variable(tf.constant(0.1, shape=[n_classes, ]))
+}
+
+
+def RNN(X, weights, biases):
+    # hidden layer for input
+    # X(128,28,28)
+    X = tf.reshape(X, [-1, n_inputs])
+    X_in = tf.matmul(X, weights['in'])+biases['in']
+    X_in = tf.reshape(X_in, [-1, n_steps, n_hidden_units])
+
+    # cell
+    lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(n_hidden_units, forget_bias=1.0, state_is_tuple=True)
+    _init_state = lstm_cell.zero_state(batch_size, dtype=tf.float32)
+    outputs, states = tf.nn.dynamic_rnn(lstm_cell, X_in, initial_state=_init_state, time_major=False)
+
+    # hidden layer for output
+    #results = tf.matmul(states[1], weights['out'])+biases['out']
+    # method 2
+    outputs = tf.unstack(tf.transpose(outputs, [1, 0, 2]))
+    results = tf.matmul(outputs[-1], weights['out'])+biases['out']
+    return results
+
+
+prediction = RNN(x, weights, biases)
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y))
+train_op = tf.train.AdamOptimizer(learning_rate).minimize(cost)
+
+
+correct_prediction = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+    step = 0
+    while step*batch_size < training_iters:
+        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+        batch_xs = batch_xs.reshape([batch_size, n_steps, n_inputs])
+        sess.run([train_op], feed_dict={x: batch_xs, y: batch_ys})
+        if step % 20 == 0:
+            print(sess.run(accuracy, feed_dict={x: batch_xs, y: batch_ys}))
+        step += 1
